767f37f0a0e66f23b14b7cb38701d46395ff0733
Yuri Rodrigues <yuri.alvess@hotmail.com>
2024-05-31 15:46:00 -0300

    projeto configurado

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -10,7 +10,7 @@ done
 
 echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
 
-python manage.py collectstatic --noinput
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-python manage.py runserver 0.0.0.0:8000
+collectstatic.sh
+makemigrations.sh
+migrate.sh
+runserver.sh
